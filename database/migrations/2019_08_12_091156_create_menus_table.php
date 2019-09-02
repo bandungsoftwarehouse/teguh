b9b05fe5c757abc4970a49428b5c70a6519f51fa
brahmantyo-dev <brahmantyo.adi@gmail.com>
2019-09-02 05:14:30 -0400

    migration file

---
+++ database/migrations/2019_08_12_091156_create_menus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text',20);
+            $table->string('description',100)->nullable();
+            $table->string('url')->default('#');
+            $table->integer('level')->default(0);
+            $table->integer('parent')->nullable();
+            $table->string('permission',50)->default('user.common');
+            $table->timestamps();
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
---
+++ database/migrations/2019_08_31_172408_create_member_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member', function (Blueprint $table) {
+	    $table->bigInteger('id_user');
+	    $table->string('plan');
+	    $table->string('status');
+	    $table->timestamp('expired_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member');
+    }
+}
