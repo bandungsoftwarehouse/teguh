75c4964a43bab9a938438eadd1cf476924f954d8
brahmantyo-dev <brahmantyo.adi@gmail.com>
2019-09-02 05:06:48 -0400

    Embed default swf file

--- resources/views/template/home.blade.php
+++ resources/views/template/home.blade.php
@@ -3,45 +3,28 @@
 @section('content')
 <!--main content start-->
 <section id="main-content">
-    <section class="wrapper site-min-height">
-        <h3><i class="fa fa-angle-right"></i> Blank Page</h3>
+    <section class="wrapper site-min-height"> 
         <div class="row mt">
             <div class="col-lg-12">
-                    {{-- <div class="card">
-                            <div class="card-header">Dashboard</div>
-            
-                            <div class="card-body">
-                                @if (session('status'))
-                                    <div class="alert alert-success" role="alert">
-                                        {{ session('status') }}
-                                    </div>
-                                @endif
-            selamat
-                                You are logged in!
-                            </div>
-                        </div> --}}
-            </div>
-        </div>
-    </section><! --/wrapper -->
-</section><!-- /MAIN CONTENT -->
-{{--   
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+		<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" 
+			codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" 
+			width="1298px" height="600px" id="tech" align="middle">
+			<param name="allowScriptAccess" value="sameDomain" />
+			<param name="movie" value="{{ localasset('swf/app1.swf') }}" /> 
+			<param name="quality" value="high" />
+			<embed src="{{ localasset('swf/app1.swf') }}" quality="high" 
+			       width="1298px" height="600px" name="tech" align="middle" allowScriptAccess="sameDomain" 
+			       type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+		</object>
+                <div class="row">
+                    <div class="card">
+                        <div class="card-body">
                         </div>
-                    @endif
-selamat
-                    You are logged in!
+                    </div> 
                 </div>
             </div>
         </div>
-    </div>
-</div> --}}
+
+    </section><! --/wrapper --> 
+</section><!-- /MAIN CONTENT -->
 @endsection
